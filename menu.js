d3af1c013b83557a1e25a716ff804c7894c77147
LucasHuls <l.j.huls@outlook.com>
2021-10-12 14:09:02 +0200

    music update

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-window.onload = function () {
-    var audio = new Audio('');
+function playmusic() {
+    var audio = new Audio('https://www.dropbox.com/s/bp49zvq2fkurnmm/music.mp3?dl=1');
     audio.play();
   }
